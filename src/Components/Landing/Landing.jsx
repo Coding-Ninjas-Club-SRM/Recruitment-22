e3cf0fd8622adb0e94aad5d38c3c04980798c6d9
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-10-06 21:35:57 +0530

    Change background to tsparticles

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import line from "./assets/line.png";
 import Timer from "./components/Timer";
 import style from "./Landing.module.css";
 
 function Landing() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
-    <div className="w-full h-screen bg-landingbg bg-cover flex flex-col justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="md:pl-[84px] py-6 md:self-start">
         <Timer className="py-4" />
         <p className="text-gray-500 text-xl xs:text-2xl flex flex-row items-center font-audiowide tracking-[0.21em] py-4">
@@ -43,6 +54,88 @@ function Landing() {
         src={line}
         alt=""
         className="w-7/10 absolute bottom-0 left-0 right-0 block ml-auto mr-auto"
+      />
+
+      <div id="particles-js"></div>
+      <script src="particles.js"></script>
+
+      <Particles
+        className="-z-10"
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#000000",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
       />
     </div>
   );
