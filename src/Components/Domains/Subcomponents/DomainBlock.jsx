f29dc4027d0440bffec2d2c5caacf918bb44cff7
Ahan <64554828+AhanR@users.noreply.github.com>
2022-10-08 00:33:26 +0530

    updated name for scroll magic compatibility

--- src/Components/Domains/Subcomponents/DomainBlock.jsx
+++ src/Components/Domains/Subcomponents/DomainBlock.jsx
@@ -3,7 +3,7 @@ import style from "../Domains.module.css";
 
 export default function DomainBlock(props) {
   return (
-    <div className={` ${style.blockParent} `}>
+    <div className={` ${style.blockParent} `} name={props.name}>
         <div>
             <h2 className={` text-[#EE4623] font-semibold text-5xl mb-[1ch] w-min mx-auto md:mx-0 ${style.textShadowRed} ${style.font}`}>[{props.title}]</h2>
             <div className={`${style.details} ${style.font} pl-[1ch] my-3 ml-2 text-xl`}>{props.details}</div>
