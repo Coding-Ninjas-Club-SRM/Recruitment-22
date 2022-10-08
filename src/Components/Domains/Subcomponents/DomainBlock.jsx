d363ec5d78bfdece8c780a979ea803e4b9aeaa17
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-10-08 23:05:30 +0530

    Update domains content

--- src/Components/Domains/Subcomponents/DomainBlock.jsx
+++ src/Components/Domains/Subcomponents/DomainBlock.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 import style from "../Domains.module.css";
 
 export default function DomainBlock(props) {
   return (
-    <div className={` ${style.blockParent} `} name={props.name}>
-        <div>
-            <h2 className={` text-[#EE4623] font-semibold text-5xl mb-[1ch] w-min mx-auto md:mx-0 ${style.textShadowRed} ${style.font}`}>[{props.title}]</h2>
-            <div className={`${style.details} ${style.font} pl-[1ch] my-3 ml-2 text-xl`}>{props.details}</div>
+    <div className={`pb-10 ${style.blockParent} `} name={props.name}>
+      <div>
+        <h2
+          className={` text-[#EE4623] font-semibold text-5xl mb-[1ch] w-min mx-auto md:mx-0 capitalize ${style.textShadowRed} ${style.font}`}
+        >
+          [{props.title}]
+        </h2>
+        <div
+          className={`${style.details} ${style.font} pl-[1ch] my-3 ml-2 text-xl`}
+        >
+          {props.details.map((e) => (
+            <p>{e}</p>
+          ))}
         </div>
-        <div className={` w-[33vw] h-[33vw] ${style.imageBlock} `}>
-            <img src={process.env.PUBLIC_URL+props.image} alt={`image for ${props.title}`} className={` w-[100%] h-[100%] `}/>
-        </div>
+      </div>
+      <div className={` w-[33vw] h-[33vw] ${style.imageBlock} `}>
+        <img
+          src={process.env.PUBLIC_URL + props.image}
+          alt={`image for ${props.title}`}
+          className={` w-[100%] h-[100%] `}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- src/Components/Domains/Subcomponents/content.jsx
+++ src/Components/Domains/Subcomponents/content.jsx
@@ -1,20 +1,57 @@
 export const domContent = [
   {
-    title: "technical",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna et elementum. Vestibulum aliquet mi quam, non imperdiet erat laoreet sit amet.",
+    title: "creatives",
+    details: [
+      "There's art in everything we see and creativity in everything our mind conceives. Here in CN, we have this domain with very creative and artistic minds. There are 3 sub domains - VFX, GFX and SFX.",
+      "1.  We design posters, templates, create posts and stories in GFX.",
+      "2.  Make reels and short videos for VFX.",
+      "3.  And, give astounding experiences by adding sound in SFX.",
+      "It gives you the comfort to come out of your dusty dorms and let the creative worm in you taste something new!",
+    ],
     image: "logo192.png",
   },
   {
-    title: "creatives",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna et elementum. Vestibulum aliquet mi quam, non imperdiet erat laoreet sit amet.",
+    title: "editorial",
+    details: [
+      "Herein, we have writers with zeal and enthusiasm who give words that reach to the people. We write content for our website, posts, stories, articles, blogs etc.",
+      "Basically, a group full of Grammar Nazis.",
+    ],
+    image: "logo192.png",
+  },
+  {
+    title: "technical",
+    details: [
+      "Be it a classy App with it's amazing UI or a website with it's superb backend-technical domain is here to help you solve every bug you'll ever have!",
+      "We have 3 subdomains, which are as follows-",
+      "1.  AI/ML- Making the use of Artificial Intelligence and Machine Learning resourceful by landing in some amazing projects.",
+      "2.  Web-Development- Designing websites and solving bugs since forever.",
+      "3.  App Development- Building Apps and correcting the UI.",
+    ],
     image: "logo192.png",
   },
   {
     title: "corporate",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna et elementum. Vestibulum aliquet mi quam, non imperdiet erat laoreet sit amet.",
+    details: [
+      "If public speaking is your niche and communicating effectively is something you're a pro at, then corporate is your place to be!",
+      "From initial ideation, to handling the event - Corporate got everything that your inner extrovert needs!",
+    ],
+    image: "logo192.png",
+  },
+  {
+    title: "sponsorship",
+    details: [
+      "Have the zeal to bring in the bucks?",
+      "Sponsorship awaits you then!",
+      "Contact the companies, bring in a deal and make yourself a pro at it! ",
+    ],
+    image: "logo192.png",
+  },
+  {
+    title: "competetive programming",
+    details: [
+      "Does the coder inside you need a platform to kickstart your journey in competitive programming?",
+      "Then, our very new addition to the club, CP domain, should be your forte.",
+    ],
     image: "logo192.png",
   },
 ];
