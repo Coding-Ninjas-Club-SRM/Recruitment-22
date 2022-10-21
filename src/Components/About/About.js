fdcd8a5d39a2390d411e78d90ade3279d1eb40d0
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-10-21 21:25:16 +0530

    Left align text in mobile view

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -28,7 +28,7 @@ function About() {
         <div className={`flex justify-center items-center ${style.imgabt}`}>
           <iframe
             className={style.video}
-            src="https://www.youtube.com/embed/5I_exRiVuQQ?controls=0"
+            src="https://www.youtube.com/embed/5I_exRiVuQQ?controls=0&loop=1"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
